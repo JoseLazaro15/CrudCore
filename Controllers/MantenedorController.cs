7f78be4a33d7c7670e938074ec2943525c007d5d
Jose Luis Lazaro <jl.lazaro15@gmail.com>
2024-10-12 12:19:18 -0600

    Add project files.

---
+++ Controllers/MantenedorController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CrudCore.Datos;
+using CrudCore.Models;
+
+namespace CrudCore.Controllers
+{
+    public class MantenedorController : Controller
+    {
+        ContactoDatos contactoDatos = new ContactoDatos();
+
+        public IActionResult Listar()
+        {
+            //La vista muestra una lista de contactos
+            var oLista = contactoDatos.Listar();
+
+            return View(oLista);
+        }
+
+        public IActionResult Guardar()
+        {
+            //Devuelve solo la vista
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Guardar(ContactoModel oContacto)
+        {
+            //Metodo recibe un objeto y guarda en BD
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var respuesta = contactoDatos.Guardar(oContacto);
+            if (respuesta)
+            {
+                return RedirectToAction("Listar");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+        public IActionResult Editar(int IdContacto)
+        {
+            //Devuelve solo la vista
+            var oContacto = contactoDatos.Obtener(IdContacto);
+            return View(oContacto);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(ContactoModel oContacto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var respuesta = contactoDatos.Editar(oContacto);
+            if (respuesta)
+            {
+                return RedirectToAction("Listar");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Eliminar(int IdContacto)
+        {
+            //Devuelve solo la vista
+            var oContacto = contactoDatos.Obtener(IdContacto);
+            return View(oContacto);
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(ContactoModel oContacto)
+        {
+
+            var respuesta = contactoDatos.Eliminar(oContacto.IdContacto);
+            if (respuesta)
+            {
+                return RedirectToAction("Listar");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
